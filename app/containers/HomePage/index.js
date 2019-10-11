932a0f0c511f551d0567fc5dcbce0fca44c9672a
Jordis Man <jordisman@gmail.com>
2019-10-10 20:11:16 -0700

    Edited homepage

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -1,18 +1,41 @@
-/*
- * HomePage
- *
- * This is the first thing users see of our App, at the '/' route
- *
- */
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
+import Person from '../../components/Person';
+import AddPerson from '../../components/AddPerson';
+import * as actionTypes from './store/actions';
 
-export default function HomePage() {
-  return (
-    <h1>
-      <FormattedMessage {...messages.header} />
-    </h1>
-  );
+class Persons extends Component {
+  render() {
+    return (
+      <div>
+        <h2>This is Home Page</h2>
+        <AddPerson personAdded={this.props.onAddedPerson} />
+        {this.props.prs.map(person => (
+          <Person
+            key={person.id}
+            name={person.name}
+            phone={person.phone}
+            clicked={() => this.props.onRemovedPerson(person.id)}
+          />
+        ))}
+      </div>
+    );
+  }
 }
+
+const mapStateToProps = state => ({
+  prs: state.persons,
+});
+
+const mapDispatchToProps = dispatch => ({
+  onAddedPerson: (name, phone) =>
+    dispatch({ type: actionTypes.ADD_PERSON, personData: { name, phone } }),
+  onRemovedPerson: id =>
+    dispatch({ type: actionTypes.REMOVE_PERSON, personId: id }),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Persons);
